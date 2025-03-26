c508910d7516c7db254abf9e4fe21857d83659c2
Musaib03 <wanimusaib520@gmail.com>
2025-03-26 22:46:17 +0530

    Add Salah model with migration and fixtures for user association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_03_26_165814) do
+ActiveRecord::Schema[8.1].define(version: 2025_03_26_171153) do
+  create_table "salahs", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "salah_name"
+    t.boolean "salah_prayed"
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_salahs_on_user_id"
+  end
+
   create_table "sessions", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "ip_address"
@@ -28,5 +37,6 @@ ActiveRecord::Schema[8.1].define(version: 2025_03_26_165814) do
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
+  add_foreign_key "salahs", "users"
   add_foreign_key "sessions", "users"
 end
