0f1bd7753841e56dfd6c51addc3aaf07e54b5953
Musaib03 <wanimusaib520@gmail.com>
2025-03-27 12:10:50 +0530

    adjust Salah routes formatting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,5 @@ class ApplicationController < ActionController::Base
     redirect_to new_session_path, alert: "Please log in to continue." unless current_user
   end
   include Authentication
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 end
