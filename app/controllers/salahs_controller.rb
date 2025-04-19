2dd0e4eefc040ed8b56354eec0ffcc45fcbda8d9
Vinit Choudhary <vinitchahal1241@gmail.com>
2025-04-19 22:50:51 +0530

    Add destroy and update actions to SalahsController with error handling

--- app/controllers/salahs_controller.rb
+++ app/controllers/salahs_controller.rb
@@ -1,5 +1,6 @@
 class SalahsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_salah, only: [:destroy, :update]
 
   def index
     selected_date = params[:date].present? ? Date.parse(params[:date]) : Time.zone.today
@@ -26,8 +27,32 @@ class SalahsController < ApplicationController
     end
   end
 
+  def destroy
+    @salah.destroy
+    redirect_to salahs_path, notice: "Salah deleted successfully."
+  end
+
+  def update
+    if params[:toggle_status]
+      @salah.update(salah_prayed: !@salah.salah_prayed)
+      redirect_to salahs_path, notice: "Salah status updated successfully."
+    else
+      if @salah.update(salah_params)
+        redirect_to salahs_path, notice: "Salah updated successfully."
+      else
+        redirect_to salahs_path, alert: "Error updating Salah."
+      end
+    end
+  end
+
   private
   def salah_params
     params.require(:salah).permit(:salah_name, :salah_prayed)
+  end
+
+  def set_salah
+    @salah = Salah.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to salahs_path, alert: "Salah not found."
   end
 end
