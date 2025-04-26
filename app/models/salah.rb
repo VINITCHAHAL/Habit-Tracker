73f78b5529b2330ab0adb8d38ab5b59f242f6291
Musaib03 <wanimusaib520@gmail.com>
2025-04-26 12:26:27 +0530

    Add consistency check for prayed_in_masjid before saving Salah

--- app/models/salah.rb
+++ app/models/salah.rb
@@ -1,3 +1,11 @@
 class Salah < ApplicationRecord
   belongs_to :user
+
+  before_save :ensure_masjid_consistency
+
+  private
+
+  def ensure_masjid_consistency
+    self.prayed_in_masjid = false if salah_prayed == false
+  end
 end
